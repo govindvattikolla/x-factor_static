4d659f90380cd279b959fb5dc24214e5bb9f4f7d
govindvattikolla <115821562+govindvattikolla@users.noreply.github.com>
2025-05-26 11:42:20 +0530

    commit-2

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ prevBtn.addEventListener('click', () => {
     track.style.transform = `translateX(${position}px)`;
   }
 });
+
+// hero section
+ const words = ["path", "pace", "potential"];
+  let i = 0;
+  const wordElement = document.getElementById("changing-word");
+
+  setInterval(() => {
+    i = (i + 1) % words.length;
+    wordElement.classList.remove("fade-in");
+    void wordElement.offsetWidth; // Reset animation
+    wordElement.textContent = words[i];
+    wordElement.classList.add("fade-in");
+  }, 2000);
+
+  // trainers
+  const slider = document.getElementById('slider');
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function updateSliderPosition() {
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  updateSliderPosition();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  updateSliderPosition();
+}
+
+// Auto-slide every 5 seconds
+setInterval(nextSlide, 5000);
